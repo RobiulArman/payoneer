60435b10ed1c646ce7f7ae8ea04e47aac243e9c7
RobiulArman <robiularman5@gmail.com>
2024-09-20 21:28:32 +0600

    done

--- js/login.js
+++ js/login.js
@@ -7,7 +7,6 @@
 // search: form submit reloading the page
 
 // step - 1 set event handler
-
 // document.getElementById('button-login')
 //     .addEventListener('click', function(event){
 //         // step-2: prevent default behavior (prevent reloading browser)
